99c5822bf0b9bd8c04cb1ed9109686503fd8bd0c
Tommy in Tongji <36354458+TommyZihao@users.noreply.github.com>
2021-04-03 20:38:10 +0800

    Update canny-interactive-demo.py

--- canny-interactive-demo.py
+++ canny-interactive-demo.py
@@ -1,20 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('shrink2.png')
+# img = cv2.imread('images/IMG_20201209_023607.jpg')
+img = cv2.imread('crack.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray,(3,3),0)
+# gray = cv2.GaussianBlur(gray,(3,3),0)
 
-cv2.namedWindow('canny demo')
+window_name = 'trackbar demo' # 显示图片窗口的名称
+cv2.namedWindow(window_name) # 窗口大小可以改变
+cv2.resizeWindow(window_name, (int(img.shape[0]/20), int(img.shape[1]/20)))
 
-cv2.createTrackbar('Min','canny demo',100, 1000, lambda x: None)
-cv2.createTrackbar('Max','canny demo',100, 1000, lambda x: None)
+cv2.createTrackbar('Min',window_name,100, 1000, lambda x: None)
+cv2.createTrackbar('Max',window_name,100, 1000, lambda x: None)
 
 try:
     while(True):
-        low_thres = cv2.getTrackbarPos('Min','canny demo')
-        max_thres = cv2.getTrackbarPos('Max','canny demo')
-        edge = cv2.Canny(blur, low_thres, max_thres)
+        low_thres = cv2.getTrackbarPos('Min',window_name)
+        max_thres = cv2.getTrackbarPos('Max',window_name)
+        result = cv2.Canny(gray, low_thres, max_thres)
         
         # 下面A、B两个代码块选一个运行
         
@@ -22,13 +25,13 @@ try:
         # dst = cv2.bitwise_and(img, img, mask=edge) 
         
         # B代码：将原图加上边缘显示
-        gray_to_bgr = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR);
+        gray_to_bgr = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR);
         gray_to_bgr[:,:,0]=gray_to_bgr[:,:,0]/255*50
         gray_to_bgr[:,:,1]=gray_to_bgr[:,:,1]/255*205
         gray_to_bgr[:,:,2]=gray_to_bgr[:,:,2]/255*50
         dst = cv2.add(img, gray_to_bgr)
         
-        cv2.imshow('canny demo', dst)
+        cv2.imshow(window_name, dst)
         if cv2.waitKey(1) in [113,27]: # 按键盘上的q或esc退出
             break
 except:
